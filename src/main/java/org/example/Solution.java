c6bc5c904b735dfa76b7da8842df97a2eed81e1d
Vyctrus <and.gierlak@gmail.com>
2023-04-15 00:15:36 +0200

    Few ideas; but using dictionary was the most natural way; I wonder If i was allowed to use this(simple hashmap solution, code will work wrong with invalid data).

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,7 +1,19 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
     public int solution(int[] A){
-        return 69;
+        Map<Integer, Boolean> dictionary= new HashMap<Integer, Boolean>();
+        for(int i=0;i<A.length;i++){
+            if(dictionary.containsKey(A[i])){
+                dictionary.remove(A[i]);
+            }else{
+                dictionary.put(A[i],true);
+            }
+        }
+        int result=dictionary.keySet().stream().findFirst().get();
+        return result;
     }
 }
